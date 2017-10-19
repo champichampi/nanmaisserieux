04ad4d24e9b2f1ece6057de3414eb1eb868e8896
thomas <caparros.thomas@yahoo.com>
2017-10-19 10:13:07 +0200

    nouveau theme
    theme wp millenium

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="<?php bloginfo( 'language' ); ?>" dir="<?php bloginfo( 'text_direction' ); ?>">
+<head <?php language_attributes(); ?>>
+	<meta charset="UTF-8">
+	<title>__TITRE_DU_SITE_ICI__</title>
+	<link href="<?php bloginfo( 'stylesheet_url' ); ?>" rel="stylesheet">		<!--CSS-->
+	<link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">		<!--Font awesome-->
+	
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="js/main.js"></script>		<!-- Javascript -->
+	<script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>		<!--jQuery-->
+
+	<!--<script src="js/lightbox/js/lightbox.min.js"></script>-->		<!--Lightbox-->
+	<!--<link href="js/lightbox/css/lightbox.min.css" rel="stylesheet" type="text/css">-->		<!--Lightbox-->
+	<!--<script src="js/mixitup.min.js"></script>-->		<!--mixitup-->
+	
+
+</head>
+
+		
+		
+<body>
+			
+		
+    <header>    <!-- En-tête de la page --> 
+        
+
+        <nav>    <!-- Navigation principale -->
+            <ul class="nav-haut line">
+                <li class="burger"><a href="#"><i class="fa fa-bars" aria-hidden="true"></i>
+                </a></li>
+                <li class="nav-haut-item millenium"> 
+                    <h1>
+                        <a href="index.html" title="Retour à l'accueil…">
+                        Millenium
+                        </a>
+                    </h1>
+                </li>
+                <li  class="nav-haut-item video"><a href="#"><i class="fa fa-television" aria-hidden="true"></i>
+                 TV / Video</a></li>
+                <li class="nav-haut-item actus"><a href="#"><i class="fa fa-address-book-o" aria-hidden="true"></i>
+                 Actualités</a></li>
+                <li class="nav-haut-item store"><a href="#">Store</a></li>
+                <li class="recherche">
+                    <form action="#" method="get">
+                        <input type="text" id="search" name="search" placeholder="Recherche">
+                        <button type="submit">
+                        <label for="search"><i class="fa fa-search" aria-hidden="true"></i>
+                        </label>
+                        </button>
+                    </form>
+                </li>
+                <li class="nav-haut-item connexion"><a href="#">Connexion</a></li>
+            </ul>
+        </nav>
+
+        <ul class="nav-gauche">
+            <li><a href="index.html">Accueil</a></li>
+            <li><a href="#" class="lol">LoL</a></li>
+            <li><a href="#" class="hs">HS</a></li>
+            <li><a href="#" class="wow">WoW</a></li>  
+            <li><a href="#" class="lol">LoL</a></li>
+            <li><a href="#" class="hs">HS</a></li>
+            <li><a href="#" class="wow">WoW</a></li>   
+            <li><a href="#" class="lol">LoL</a></li>
+            <li><a href="#" class="hs">HS</a></li>
+            <li><a href="#" class="wow">WoW</a></li>   
+            <li><a href="#" class="lol">LoL</a></li>
+            <li><a href="#" class="hs">HS</a></li>
+            <li><a href="#" class="wow">WoW</a></li> 
+        </ul>
+
+    </header>
+
+
+    
+    <div class="lecteur contenu">
+    	<p class="fermer-lecteur"><a href="#">fermer le player <i class="fa fa-caret-down" aria-hidden="true"></i></a></p>
+    	
+    	<div class="stream">
+    		<div class="stream-video">
+    			<img src="img/jonsnow.jpg" alt="">
+    		</div>
+    		<div class="liste-stream">
+				<ul>
+					<li><a href="#"><span class="liste-stream-icon">wow</span><span class="liste-stream-texte">zegezge</span></a></li>
+					<li><a href="#"><span class="liste-stream-icon">lol</span><span class="liste-stream-texte">zegezge</span></a></li>
+					<li><a href="#"><span class="liste-stream-icon">hs</span><span class="liste-stream-texte">zegezge</span></a></li>
+					<li><a href="#"><span class="liste-stream-icon">diablo</span><span class="liste-stream-texte">zegezge</span></a></li>
+					<li><a href="#"><span class="liste-stream-icon">CS</span><span class="liste-stream-texte">zegezge</span></a></li>
+					<li><a href="#"><span class="liste-stream-icon">HoTS</span><span class="liste-stream-texte">zegezge</span></a></li>
+				</ul>
+				<a href="#"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i> Programme TV</a>
+    		</div>
+    		
+    	</div>
+    	
+    </div>
+    
+
+    <div class="contenu">
+	   
+	   <p class="ouvrir-lecteur"><a href="#">ouvrir le player <i class="fa fa-caret-up" aria-hidden="true"></i></a></p>
+
+       
+       
+        <p class="accueil">
+            <strong>Bienvenue sur millenium.org.</strong> Retrouvez toutes les compétitions esport sur notre web-TV et découvrez toute l'actualité de League of Legends, Hearthstone et World of Warcraft.
+        </p>
+        <div class="main-aside">
+            <main>    <!-- main -->
+                <div class="article">
+                    <div class="article-h2">
+                        <h2>Millenium</h2>
+                    </div>
+                    <div class="article-h2 span-h2">
+                        <h2>Z Event</h2>
+                    </div>
+                    <div class="article-contenu">
+                        <a href="#" class="a-article">
+							<div class="contour-img"><img src="img/maxresdefault.jpg" alt=""></div>
+                            <h3>Z-Event : Déjà 140 000€ récoltés !</h3>
+                            <p class="date-et-heure">date et heure de l'article</p> 
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste fugiat quasi voluptatum suscipit, vero nisi hic porro possimus officia corporis.</p>
+                        </a>
+                    </div>
+                    <div class="tout-sur"><a href="#">Tout sur Millenium</a></div>
+                </div>
+            
+
+            </main>
+            
+            
+            
+
+            <aside>
+                <div class="aside-actu">
+					<h2><span class="titre-flash-info"><i class="fa fa-clock-o" aria-hidden="true"></i>
+						 Flash info</span></h2>
+					<ul class="flash-info">
+						<li><a href="#"><span class="heure">Heure</span> <span class="jeu">jeu</span> - <span class="titre-news">titre de la news</span></a></li>
+						<li><a href="#"><span class="heure">Heure</span> <span class="jeu">jeu</span> - <span class="titre-news">titre de la news</span></a></li>
+						<li><a href="#"><span class="heure">Heure</span> <span class="jeu">jeu</span> - <span class="titre-news">titre de la news</span></a></li>
+					</ul>
+					<p>
+					<a href="#"><i class="fa fa-arrow-circle-right" aria-hidden="true"></i> Voir toute l'actu</a></p>
+                </div>
+                
+                <div class="aside-decouverte aside-article-margin">
+                	<h2><span>A découvrir</span></h2>
+					<ul class="a-decouvrir">
+						<li>
+							<a href="#">
+								<div class="contour-img"><img src="img/hqdefault.jpg" alt=""></div>
+								<p><span class="rubrique">Guide</span></p>
+								<p><span class="sujet">Sujet</span></p>
+							</a>
+						</li> 
+						<li>
+							<a href="#">
+								<div class="contour-img"><img src="img/benzaie.jpg" alt=""></div>
+								<p><span class="rubrique">Guide</span></p>
+								<p><span class="sujet">Sujet</span></p>
+							</a>
+						</li> 
+						<li>
+							<a href="#">
+								<div class="contour-img"><img src="img/maxresdefault-1.jpg" alt=""></div>
+								<p><span class="rubrique">Guide</span></p>
+								<p><span class="sujet">Sujet</span></p>
+							</a>
+						</li>
+
+					</ul>
+                </div>
+                
+                <div class="aside-decouverte aside-article-margin">
+                	<h2><span>Vidéos</span></h2>
+					<ul class="a-decouvrir">
+						<li>
+							<a href="#">
+								<div class="contour-img">video</div>
+								<p><span class="rubrique">Guide</span></p>
+								<p><span class="sujet">Sujet</span></p>
+							</a>
+						</li> 
+						<li>
+							<a href="#">
+								<div class="contour-img">video</div>
+								<p><span class="rubrique">Guide</span></p>
+								<p><span class="sujet">Sujet</span></p>
+							</a>
+						</li> 
+						<li>
+							<a href="#">
+								<div class="contour-img">video</div>
+								<p><span class="rubrique">Guide</span></p>
+								<p><span class="sujet">Sujet</span></p>
+							</a>
+						</li>
+
+					</ul>
+                </div>
+            </aside>
+        </div>
+        
+        
+        <footer>    <!-- Pied de page -->
+            <a href="#"><img src="img/footer-webedia.png" alt=""></a>
+            <ul>
+            	<li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+            	<li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+            	<li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>
+				</a></li>
+				<li><a href="#">Mentions légales
+				</a></li>
+            </ul>
+        </footer>
+    </div>
+		
+	
+</body>
+</html>
